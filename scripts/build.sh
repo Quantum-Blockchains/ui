05dedd65d03e7c596173e1f60c4b446fb65dd8ca
kostiask <kostiaskopec@gmail.com>
2023-10-17 19:57:08 +0200

    update script build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,23 +13,23 @@ rm -r ../../node_modules/@polkadot/wasm-crypto-asmjs
 rm -r ../../node_modules/@polkadot/wasm-crypto-init
 rm -r ../../node_modules/@polkadot/wasm-crypto-wasm
 rm -r ../../node_modules/@polkadot/wasm-util
-rm -r node_modules/@polkadot/keyring
-rm -r node_modules/@polkadot/networks
-rm -r node_modules/@polkadot/util
-rm -r node_modules/@polkadot/util-crypto
-rm -r node_modules/@polkadot/x-bigint
-rm -r node_modules/@polkadot/x-bundle
-rm -r node_modules/@polkadot/x-global
-rm -r node_modules/@polkadot/x-randomvalues
-rm -r node_modules/@polkadot/x-textdecoder
-rm -r node_modules/@polkadot/x-textencoder
+rm -r ../../node_modules/@polkadot/keyring
+rm -r ../../node_modules/@polkadot/networks
+rm -r ../../node_modules/@polkadot/util
+rm -r ../../node_modules/@polkadot/util-crypto
+rm -r ../../node_modules/@polkadot/x-bigint
+rm -r ../../node_modules/@polkadot/x-bundle
+rm -r ../../node_modules/@polkadot/x-global
+rm -r ../../node_modules/@polkadot/x-randomvalues
+rm -r ../../node_modules/@polkadot/x-textdecoder
+rm -r ../../node_modules/@polkadot/x-textencoder
 
-cp -r node_modules/@polkadot/wasm-bridge/build ../../node_modules/@polkadot/wasm-bridge
-cp -r node_modules/@polkadot/wasm-crypto/build ../../node_modules/@polkadot/wasm-crypto
-cp -r node_modules/@polkadot/wasm-crypto-asmjs/build ../../node_modules/@polkadot/wasm-crypto-asmjs
-cp -r node_modules/@polkadot/wasm-crypto-init/build ../../node_modules/@polkadot/wasm-crypto-init
-cp -r node_modules/@polkadot/wasm-crypto-wasm/build ../../node_modules/@polkadot/wasm-crypto-wasm
-cp -r node_modules/@polkadot/wasm-util/build ../../node_modules/@polkadot/wasm-util
+cp -r node_modules/@polkadot/wasm-bridge ../../node_modules/@polkadot/wasm-bridge
+cp -r node_modules/@polkadot/wasm-crypto ../../node_modules/@polkadot/wasm-crypto
+cp -r node_modules/@polkadot/wasm-crypto-asmjs ../../node_modules/@polkadot/wasm-crypto-asmjs
+cp -r node_modules/@polkadot/wasm-crypto-init ../../node_modules/@polkadot/wasm-crypto-init
+cp -r node_modules/@polkadot/wasm-crypto-wasm ../../node_modules/@polkadot/wasm-crypto-wasm
+cp -r node_modules/@polkadot/wasm-util ../../node_modules/@polkadot/wasm-util
 cp -r packages/keyring/build ../../node_modules/@polkadot/keyring
 cp -r packages/networks/build ../../node_modules/@polkadot/networks
 cp -r packages/util/build ../../node_modules/@polkadot/util
